17f7da0d89a6f2275e3c9637d930884df45e5f4d
Rochelle Roberts <rochelle@>
2019-06-26 16:32:43 -0700

    deleted some comments

--- src/coolDate.js
+++ src/coolDate.js
@@ -14,17 +14,3 @@ export class CoolDate extends Date{
     return num;
   }
 }
-
-
-
-
-
-
-// Y is the year minus 1 for January or February, and the year for any other month
-// y is the last 2 digits of Y
-// c is the first 2 digits of Y
-// d is the day of the month (1 to 31)
-// m is the shifted month (March=1,...,February=12)
-// w is the day of week (0=Sunday,...,6=Saturday). If w is negative you have to add 7 to it.
-
-// let monthDictionary = ["no", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February"]
